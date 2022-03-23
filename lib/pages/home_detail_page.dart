d6525eff118390657bdde200636a4db583ef8b90
ishwar46 <ishwarura@gmail.com>
2022-03-23 17:23:08 +0545

    Create home_detail_page.dart

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/models/catalog.dart';
+import 'package:flutter_practice/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+
+  const HomeDetailPage({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.color(Colors.green).bold.xl2.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    MyTheme.darkBluishColor,
+                  ),
+                  shape: MaterialStateProperty.all(
+                    StadiumBorder(),
+                  )),
+              child: "Buy".text.light.make(),
+            ).wh(100, 50)
+          ],
+        ).p16(),
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context),
+            Expanded(
+              child: VxArc(
+                height: 30.0,
+                arcType: VxArcType.CONVEY,
+                edge: VxEdge.TOP,
+                child: Container(
+                  color: Colors.white,
+                  width: context.screenWidth,
+                  child: Column(
+                    children: [
+                      catalog.name.text.xl2
+                          .color(MyTheme.darkBluishColor)
+                          .bold
+                          .make(),
+                      catalog.description.text
+                          .color(Colors.lightBlue)
+                          .xl
+                          .make(),
+                      10.heightBox,
+                    ],
+                  ).py64(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
