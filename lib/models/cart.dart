e7018e13ba67450af48aaf0c8a304b25d744c0cd
ishwar46 <ishwarura@gmail.com>
2022-03-25 20:23:35 +0545

    Create cart.dart

---
+++ lib/models/cart.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_practice/models/catalog.dart';
+
+class CartModel {
+  //catalog fiels
+  late CatalogModel _catalog;
+
+  //collections of IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  //get catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  // get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add Items
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove Items
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
