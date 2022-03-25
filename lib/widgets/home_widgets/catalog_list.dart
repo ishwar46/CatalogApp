0d5384eae6ffae584317935a38020aa7c51f6fc0
ishwar46 <ishwarura@gmail.com>
2022-03-25 22:34:23 +0545

    Update catalog_list.dart

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/models/cart.dart';
+//import 'package:flutter_practice/models/cart.dart';
 import 'package:flutter_practice/models/catalog.dart';
 import 'package:flutter_practice/pages/home_detail_page.dart';
-import 'package:flutter_practice/widgets/themes.dart';
+import 'package:flutter_practice/widgets/home_widgets/add_to_cart.dart';
+//import 'package:flutter_practice/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import 'catalog_image.dart';
@@ -61,7 +62,7 @@ class CatalogItem extends StatelessWidget {
                 buttonPadding: EdgeInsets.zero,
                 children: [
                   "\$${catalog.price}".text.color(Colors.green).bold.lg.make(),
-                  _AddToCart(catalog: catalog)
+                  AddToCart(catalog: catalog)
                 ],
               ).pOnly(right: 8.0)
             ],
@@ -69,43 +70,5 @@ class CatalogItem extends StatelessWidget {
         ],
       ),
     ).color(context.cardColor).rounded.square(150).make().py16();
-  }
-}
-
-class _AddToCart extends StatefulWidget {
-  final Item catalog;
-  const _AddToCart({
-    Key? key,
-    required this.catalog,
-  }) : super(key: key);
-
-  @override
-  State<_AddToCart> createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<_AddToCart> {
-  final _cart = CartModel();
-  @override
-  Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
-    return ElevatedButton(
-      onPressed: () {
-        if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
-        }
-      },
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(
-            MyTheme.darkBluishColor,
-          ),
-          shape: MaterialStateProperty.all(
-            StadiumBorder(),
-          )),
-      child: isInCart ? Icon(Icons.done) : "Add to cart".text.light.make(),
-    );
   }
 }
