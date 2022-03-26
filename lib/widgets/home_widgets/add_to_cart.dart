0b56d6a9899adc7f0c5fd3d886ae1e0e569d46b8
ishwar46 <ishwarura@gmail.com>
2022-03-26 10:28:38 +0545

    Update add_to_cart.dart

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -15,14 +15,13 @@ class AddToCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    VxState.watch(context, on: [AddMutation]);
+    VxState.watch(context, on: [AddMutation, RemoveMutation]);
     final CartModel _cart = (VxState.store as MyStore).cart;
     bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
           AddMutation(catalog);
-          // setState(() {});
         }
       },
       style: ButtonStyle(
