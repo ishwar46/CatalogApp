d44d53fdfc2dff0ae805ddd8b24e7a0aebce42d9
ishwar46 <ishwarura@gmail.com>
2022-03-26 10:03:06 +0545

    AddMutation

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_practice/core/store.dart';
 import 'package:flutter_practice/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   // static final cartModel = CartModel._internal();
@@ -38,5 +40,15 @@ class CartModel {
 
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store?.cart._itemIds.add(item.id);
   }
 }
